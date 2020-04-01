24958c96689c6205deb14679ff3d652fcfaa0ce3
Adria Pujol <xustaboy@gmail.com>
2020-04-01 21:12:02 +0200

    render products, add products to cart

---
+++ script.js
@@ -0,0 +1,132 @@
+
+window.onload = function ready() {
+  
+    product_0 = new Product("0", "White t-shirt 0", 12.99, "img/00_419571.jpg", "img/shirt_small.jpg");
+    product_1 = new Product("1", "White t-shirt 1", 13.99, "img/00_419571.jpg", "img/shirt_small.jpg");
+    product_2 = new Product("2", "White t-shirt 2", 15.99, "img/00_419571.jpg", "img/shirt_small.jpg");
+    product_3 = new Product("3", "White t-shirt 3", 18.99, "img/00_419571.jpg", "img/shirt_small.jpg");
+    const products = [product_0, product_1, product_2, product_3],
+          cart = [],
+          container = document.getElementById("products"),
+          cartHTML = this.document.getElementById("cart");  
+
+    renderProduct(products, container);
+
+    let btnAdd_0 = document.getElementById("btn-add-0");
+    let btnAdd_1 = document.getElementById("btn-add-1");
+    let btnAdd_2 = document.getElementById("btn-add-2");
+    let btnAdd_3 = document.getElementById("btn-add-3");
+
+
+    console.log(cart);
+    btnAdd_0.addEventListener('click', ()=>{
+        addCart(cart, product_0);
+        renderCart(product_0, cartHTML);
+    });
+    btnAdd_1.addEventListener('click', ()=>{
+        addCart(cart, product_1, cartHTML);
+        renderCart(product_1, cartHTML);
+    });
+    btnAdd_2.addEventListener('click', ()=>{
+        addCart(cart, product_2, cartHTML);
+        renderCart(product_2, cartHTML);
+    });
+    btnAdd_3.addEventListener('click', ()=>{
+        addCart(cart, product_3, cartHTML);
+        renderCart(product_3, cartHTML);
+    });
+
+
+
+};
+
+
+
+class Product {
+    constructor(id, name, price, img, img_s) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.img = img;
+        this.img_s = img_s;
+    }
+}
+
+const addCart = (cart, item) => {
+    cart.push(item);
+    console.log(cart);
+};
+
+const renderProduct = (list, container) => {
+    for (item of list) {
+        container.innerHTML += `<div class="product">
+                                <img src="${item.img}" class="product-${item.id}" alt="shirt">
+                                <div class="item-info">
+                                    <span class="item-name" id="item-name-${item.id}">${item.name}</span>
+                                    <span class="price" id="price-${item.id}">${item.price}</span>
+                                    <button class="btn-add" id="btn-add-${item.id}">add to cart</button>
+                                </div>
+                            </div>`
+                            ;
+    };
+};    
+
+const renderCart = (prod, cart) => {
+    cart.innerHTML += `<div class="cart-item" id="item-${prod.id}">
+                            <img src="${prod.img_s}" class="cart-img" alt="item cart">
+                            <span>${prod.name}</span>
+                            <input class="cart-quantity-input" type="number" min="0" max="20" value="0">
+                            <span class="cart-price">${prod.price}</span>
+                            <button class="btn-remove-cart">X</button>
+                        </div>`
+                        ;
+};    
+
+
+
+
+// let btnAdd = document.getElementsByClassName("btn-add");
+// console.log(btnAdd.length);
+// console.log(btnAdd);
+
+
+
+
+// let btnAdd_0 = document.getElementById("btn-add-0");
+// let btnAdd_1 = document.getElementById("btn-add-1");
+// let btnAdd_2 = document.getElementById("btn-add-2");
+// let btnAdd_3 = document.getElementById("btn-add-3");
+// let btnTest = document.getElementsByClassName("btn-add")[0];
+// let itemTest = document.getElementById("item-0");
+
+// // btnTest.addEventListener('click', () => {
+//     console.log(itemTest[2].value);
+// // });
+
+// btnAdd_0.addEventListener('click', ()=> {
+//     console.log("working 0");
+// });
+// btnAdd_1.addEventListener('click', ()=> {
+//     console.log("working 1");
+// });
+// btnAdd_2.addEventListener('click', ()=> {
+//     console.log("working 2");
+// });
+// btnAdd_3.addEventListener('click', ()=> {
+//     console.log("working 3");
+// });
+
+
+// let addCart = () => {
+
+
+// };
+
+
+// `<div class="cart-item">
+//     <img src="img/shirt_small.jpg" class="cart-img" alt="item cart">
+//     <span>White T-Shirt</span>
+//     <input class="cart-quantity-input" type="number" min="0" max="20" value="0">
+//     <span class="cart-price">12,99</span>
+//     <button class="btn-remove-cart">X</button>
+// </div>`
