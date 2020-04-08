0ba46c8f9608551108bfe1d5cc2eaf5ab9db40b8
Adria Pujol <xustaboy@gmail.com>
2020-04-08 17:50:22 +0200

    removing items from cart working, still needs to work on Total Exp

--- script.js
+++ script.js
@@ -115,11 +115,16 @@ const UIController = (() => {
             ca.innerHTML += `<div class="cart-item" id="item-${d.products[i].id}">
                                     <img src="img/shirt_s_${d.products[i].id}.jpg" class="cart-img" alt="item cart">
                                     <span>${d.products[i].name}</span>
-                                    <input class="cart-quantity-input" id="quantity-${d.products[i].id}" type="number" min="1" value="${d.cart[d.cart.indexOf(d.products[i])].quantity}">
+                                    <input class="cart-quantity-input" id="quantity-${d.products[i].id}" name="quantity-${d.products[i].id}" type="number" min="1" value="${d.cart[d.cart.indexOf(d.products[i])].quantity}">
                                     <span class="cart-price" id="cart-price-${d.products[i].id}">${d.products[i].price}</span>
                                     <button class="btn-remove-cart" id="btn-remove-${d.products[i].id}">X</button>
                                 </div>`
                                 ;
+        },
+        
+        deleteCartItem: (selectorID) => {
+            const el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
         },
 
         getDOMstrings: () => DOMstrings,
@@ -137,6 +142,8 @@ const UIController = (() => {
 //GLOBAL APP CONTROLLER
 const controller = ((shopCtrl, UICtrl) => {
 
+    //Getting DOM strings
+
     const DOM = UICtrl.getDOMstrings(),
           cart = document.getElementById(DOM.cart),
           products = document.getElementById(DOM.products),
@@ -145,10 +152,15 @@ const controller = ((shopCtrl, UICtrl) => {
     // const cart = shopCtrl.getCart();
           data = shopCtrl.getData();
    
+    //Setting up event listeners
 
     const setupeEventListeners = () => {
 
+        //Display products
+
         UICtrl.renderProducts(data.products, products);
+
+        //Add event listeners to each "add to cart" button
 
         document.getElementById(DOM.btnAdd_0).addEventListener('click', ()=>{
             addItemCart(data, cart, 0);
@@ -163,7 +175,7 @@ const controller = ((shopCtrl, UICtrl) => {
             addItemCart(data, cart, 3); 
         });
 
-
+          cart.addEventListener('click', delItemCart);
 
     };
 
@@ -174,26 +186,63 @@ const controller = ((shopCtrl, UICtrl) => {
         // UICtrl.renderCart(d.products[id], c);
         // console.log(d.total);
 
+        //Check if item is already in the cart
+
         if (d.cart.indexOf(d.products[id]) > -1) {
             const cartID = d.cart.indexOf(d.products[id]);
             console.log('WORKS', d.cart.indexOf(d.products[id]));
+            // 1. Update quantity of product in the cart
             d.cart[cartID].quantity += 1;
             console.log(d.cart[cartID].quantity);
+            // 2. Display and update value of input
             document.getElementById(`quantity-${id}`).value = d.cart[cartID].quantity;
+            // 3. Update the price of the item in the cart depending on the quantity of items added
             document.getElementById(`cart-price-${id}`).innerHTML = d.cart[cartID].price * d.cart[cartID].quantity;
+            console.log('is this working?',document.getElementById(`quantity-${id}`).value);
+            // 4. Add to total
+
+            // 5. Update total UI
             
         } else {
+
+            // 1. Add item to cart
             shopCtrl.addCart(d, id);
+
+            // 2. Update Cart UI
             UICtrl.renderCart(d, c, id);
             console.log(d.total);
+
+            // 4. Add to total
+
+            // 5. Update total UI
         }
     };    
 
     // Delete item from cart
+        const delItemCart = (event) => {
+            const itemID = event.target.parentNode.id,
+                  splitID = itemID.split('-'),
+                  numID = splitID[1],
+                  targetID = event.target.id;
+                  console.log(targetID);
+            console.log(numID);
+            console.log(event.target.parentNode);
+            console.log(itemID);
+            
+            if (itemID && targetID===`btn-remove-${numID}`) {
+                    // 1. Remove item from data cart
+                    shopCtrl.removeCart(data.cart, data.products[numID]);
+                    console.log(data.cart);
+                    // // 2. Remove item from UI
+                    UICtrl.deleteCartItem(itemID);
 
-    const delItemCart = (c, p) => {
-        shopCtrl.removeCart(c, p);
-    }
+
+            }
+        };
+    // const delItemCart = (c, p) => {
+
+    
+    // }
 
     console.log(data);
 
@@ -214,92 +263,5 @@ const controller = ((shopCtrl, UICtrl) => {
 })(shopController, UIController);
 
 controller.init();
-
-// window.onload = function ready() {
-  
-//     product_0 = new Product("0", "Dreadful", 12.99);
-//     product_1 = new Product("1", "Cat face", 13.99);
-//     product_2 = new Product("2", "Sr. Batman", 15.99);
-//     product_3 = new Product("3", "Black Mamba", 18.99);
-//     const products = [product_0, product_1, product_2, product_3],
-//           cart = [],
-//           container = document.getElementById("products"),
-//           cartHTML = document.getElementById("cart");  
-
-//     renderProduct(products, container);
-
-//     const btnAdd_0 = document.getElementById("btn-add-0"),
-//           btnAdd_1 = document.getElementById("btn-add-1"),
-//           btnAdd_2 = document.getElementById("btn-add-2"),
-//           btnAdd_3 = document.getElementById("btn-add-3");
-
-
-//     console.log(cart);
-    // btnAdd_0.addEventListener('click', ()=>{
-    //     addCart(cart, product_0);
-    //     renderCart(product_0, cartHTML);
-    // });
-//     btnAdd_1.addEventListener('click', ()=>{
-//         addCart(cart, product_1, cartHTML);
-//         renderCart(product_1, cartHTML);
-//     });
-//     btnAdd_2.addEventListener('click', ()=>{
-//         addCart(cart, product_2, cartHTML);
-//         renderCart(product_2, cartHTML);
-//     });
-//     btnAdd_3.addEventListener('click', ()=>{
-//         addCart(cart, product_3, cartHTML);
-//         renderCart(product_3, cartHTML);
-//     });
-
-
-
-// };
-
-
-
-// class Product {
-//     constructor(id, name, price) {
-//         this.id = id;
-//         this.name = name;
-//         this.price = price;
-//     }
-// }
-
-// const addCart = (cart, item) => {
-//     cart.push(item);
-//     console.log(cart);
-// };
-
-// const removeCart = (cart, item) => {
-    
-
-
-// }
-
-// const renderProduct = (list, container) => {
-//     for (item of list) {
-//         container.innerHTML += `<div class="product">
-//                                 <img src="img/shirt_${item.id}.jpg" class="product-${item.id}" alt="shirt">
-//                                 <div class="item-info">
-//                                     <span class="item-name" id="item-name-${item.id}">${item.name}</span>
-//                                     <span class="price" id="price-${item.id}">${item.price}</span>
-//                                     <button class="btn-add" id="btn-add-${item.id}">add to cart</button>
-//                                 </div>
-//                             </div>`
-//                             ;
-//     };
-// };    
-
-// const renderCart = (prod, cart) => {
-//     cart.innerHTML += `<div class="cart-item" id="item-${prod.id}">
-//                             <img src="img/shirt_s_${prod.id}.jpg" class="cart-img" alt="item cart">
-//                             <span>${prod.name}</span>
-//                             <input class="cart-quantity-input" type="number" min="1" max="20" value="1">
-//                             <span class="cart-price">${prod.price}</span>
-//                             <button class="btn-remove-cart" id="btn-remove-${item.id}">X</button>
-//                         </div>`
-//                         ;
-// };    
 
 
